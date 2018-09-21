0582327a076aee228c7e37239838dadb4069901d
Shawn Presser <shawnpresser@gmail.com>
2018-09-21 03:52:46 -0500

    Fix repl require on JS

--- packages/lumen-language/index.js
+++ packages/lumen-language/index.js
@@ -1,12 +1,16 @@
 #!/usr/bin/env node
 const path = require('path');
+const Module = require('module').Module;
 process.env.NODE_PATH = (process.env.NODE_PATH || "") + ":" + path.join(__dirname, 'bin');
+Module._initPaths();
 if (require.main === module) {
-  module.paths = require('module').Module._nodeModulePaths(path.resolve('repl'));
+  module.filename = path.resolve('repl');
+  module.paths = Module._nodeModulePaths(module.filename);
 }
-require('module').Module._initPaths();
-global.require = require;
-Object.assign(exports, require("./bin/lumen.js"));
+if (!global.hasOwnProperty('require')) {
+  global.require = require;
+}
+Object.assign(exports, require(path.join(__dirname, "bin", "lumen.js")));
 if (require.main === module) {
   exports.main();
 }
