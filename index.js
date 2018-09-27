1bcd3225e0a0a34313b04e0b3bc63ace4733cc56
Shawn Presser <shawnpresser@gmail.com>
2018-09-26 23:54:09 -0500

    index.js: Use path.delimiter in NODE_PATH

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
 const path = require('path');
-process.env.NODE_PATH = (process.env.NODE_PATH || "") + ":" + path.join(__dirname, 'bin');
+process.env.NODE_PATH = (process.env.NODE_PATH || "") + path.delimiter + path.join(__dirname, 'bin');
 if (require.main === module) {
   module.paths = require('module').Module._nodeModulePaths(path.resolve('repl'));
 }
